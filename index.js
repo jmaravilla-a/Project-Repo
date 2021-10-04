a3327e864db913a8adf41d8ab98ee8a00e0b5a08
abdulrahman muse <abdulrahmanmuse1@gmail.com>
2021-10-04 16:12:01 -0500

    added a few buttons

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //hey guys
-
+let points1 = 0
+let points2 = 0
+let points3 = 0
 let baseUrl = 'https://dog.ceo/api/breeds/image/random'
 let dogImage = document.getElementById('dog-image')
 // let attack
@@ -16,3 +18,23 @@ function renderImage(data) {
     dogImage.src = data.message
 }
 
+let attackBttn = document.getElementById("attackBttn")
+attackBttn.addEventListener("click", addAttack )
+function addAttack() { 
+    let attackPoints = document.getElementById("attack-points")
+    attackPoints.textContent = `attack: ${++points1}`
+}
+
+let defenseBttn = document.getElementById("defenseBttn")
+defenseBttn.addEventListener("click", addDefense )
+function addDefense() { 
+    let defensePoints = document.getElementById("defense-points")
+    defensePoints.textContent = `defense: ${++points2}`
+}
+
+let speedBttn = document.getElementById("speedBttn")
+speedBttn.addEventListener("click", addSpeed )
+function addSpeed() { 
+    let speedPoints = document.getElementById("speed-points")
+    speedPoints.textContent = `speed: ${++points3}`
+}
