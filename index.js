c1be3a23193b26f713eaba911473675aea42f468
Javier Maravilla <javier.maravilla@outlook.com>
2021-10-04 14:33:48 -0400

    Added glow and frame box in css. Set img width to auto

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-<<<<<<< HEAD
-=======
 //hey guys
+
 let baseUrl = 'https://dog.ceo/api/breeds/image/random'
 let dogImage = document.getElementById('dog-image')
+// let attack
 fetch(baseUrl) 
     .then(resp => resp.json())
-    .then(data => renderImage(data))
+    .then(data => renderImage(data)
+    )
+    
 
 function renderImage(data) {
     //function takes image from data
-    console.log(dogImage)
+    // console.log(dogImage)
     //replaces our dog image with data.image
     dogImage.src = data.message
 }
->>>>>>> adee83f954766788cb87409ecfb42511a4685995
+
