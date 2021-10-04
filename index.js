adee83f954766788cb87409ecfb42511a4685995
Javier Maravilla <javier.maravilla@outlook.com>
2021-10-04 12:52:03 -0400

    Added dog image from Json

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //hey guys
+let baseUrl = 'https://dog.ceo/api/breeds/image/random'
+let dogImage = document.getElementById('dog-image')
+fetch(baseUrl) 
+    .then(resp => resp.json())
+    .then(data => renderImage(data))
+
+function renderImage(data) {
+    //function takes image from data
+    console.log(dogImage)
+    //replaces our dog image with data.image
+    dogImage.src = data.message
+}
